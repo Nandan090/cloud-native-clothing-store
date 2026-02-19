78d0c6cd3b50c2bf01399996ff22d1581a7c2360
Nandan090 <155322711+Nandan090@users.noreply.github.com>
2026-02-20 00:20:04 +0200

    Refactor get_conn to include retry logic
    Refactored get_conn function to include retry logic for database connection attempts.

--- app/db_migration.py
+++ app/db_migration.py
@@ -2,19 +2,7 @@ import os, time, psycopg
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-def get_conn():
-    for attempt in range(1, 6):
-        try:
-            conn = psycopg.connect(DATABASE_URL, autocommit=True)
-            print(f"INFO: DB connection successful.")
-            return conn
-        except Exception as e:
-            print(f"WARNING: DB not ready, retrying...")
-            time.sleep(1)
-    raise Exception("DB connection failed after 5 attempts")  # ADD THIS LINE
-
 # def get_conn():
-#     # Retry loop in case postgres is not ready before app
 #     for attempt in range(1, 6):
 #         try:
 #             conn = psycopg.connect(DATABASE_URL, autocommit=True)
@@ -23,6 +11,18 @@ def get_conn():
 #         except Exception as e:
 #             print(f"WARNING: DB not ready, retrying...")
 #             time.sleep(1)
+#     raise Exception("DB connection failed after 5 attempts")  # ADD THIS LINE
+
+def get_conn():
+    # Retry loop in case postgres is not ready before app
+    for attempt in range(1, 6):
+        try:
+            conn = psycopg.connect(DATABASE_URL, autocommit=True)
+            print(f"INFO: DB connection successful.")
+            return conn
+        except Exception as e:
+            print(f"WARNING: DB not ready, retrying...")
+            time.sleep(1)
 
 # Migration: create / update database schema
 def migrate_schema():
